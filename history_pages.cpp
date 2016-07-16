0d3b9d7b526aa955b7592b4f3470c0d956a6f13a
Darrell Wright <dwright-git@dawdevel.ca>
2016-07-16 19:17:33 -0400

    .

---
+++ history_pages.cpp
@@ -0,0 +1,219 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2014-2015 Darrell Wright
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files( the "Software" ), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <boost/optional.hpp>
+#include <daw/daw_range.h>
+#include <sstream>
+#include <tuple>
+#include "history_pages.h"
+
+namespace daw {
+	namespace history {
+		namespace {
+			template<typename To, typename From>
+			To convert_to( From const & value ) {
+				std::stringstream ss;
+				ss << value;
+				To result;
+				ss >> result;
+				return result;
+			}
+		}	// namespace anonymous
+
+		pump_model_t::pump_model_t( std::string const & model ):
+				generation( convert_to<uint16_t>( model )%100u ),
+				larger{ generation >= 23 }, 
+				has_low_suspend{ generation >= 51 },
+				strokes_per_unit( generation >= 23 ? 40 : 10 ) { }
+
+		history_entry_obj::history_entry_obj( data_source_t data, size_t data_size, pump_model_t ): 
+				m_opcode{ data[0] },
+				m_data{ data.shrink( data_size ) },
+				m_size{ data_size } { }
+
+		history_entry_obj::~history_entry_obj( ) { };
+
+		std::tuple<uint8_t, size_t, size_t> history_entry_obj::register_event_type( ) const {
+			return std::make_tuple( this->opcode( ), this->size( ), this->timestamp_offset( ) );
+		}
+
+		boost::optional<timestamp_t> history_entry_obj::timestamp( ) const {
+			switch( this->timestamp_size( ) ) {
+			case 2:
+				return timestamp_t::parse_date( m_data.slice( this->timestamp_offset( ) ) );
+			case 5:
+				return timestamp_t::parse_timestamp( m_data.slice( this->timestamp_offset( ) ) );
+			default:
+				return boost::optional<timestamp_t>{ };
+			}
+		}
+
+		uint8_t history_entry_obj::opcode( ) const {
+			return m_opcode;	
+		}
+			
+		data_source_t & history_entry_obj::data( ) {
+			return m_data;
+		}	
+
+		data_source_t const & history_entry_obj::data( ) const {
+			return m_data;
+		}
+
+		size_t & history_entry_obj::size( ) {
+			return m_size;
+		}	
+
+		size_t const & history_entry_obj::size( ) const {
+			return m_size;
+		}
+
+		hist_bolus_normal::~hist_bolus_normal( ) { }
+		hist_result_daily_total::~hist_result_daily_total( ) { }
+
+		bool timestamp_t::has_time( ) const {
+			return static_cast<bool>(time);
+		}
+
+		timestamp_t timestamp_t::parse_timestamp( data_source_t arry ) {
+			assert( arry.size( ) >= 5 );
+			timestamp_t result;
+			result.time = time_t{ };
+			result.time->second = arry[0] & 0b00111111;
+			result.time->minute = arry[1] & 0b00111111;
+			result.time->hour = arry[2] & 0b00011111;
+			result.date.day = arry[3] & 0b00011111;
+			result.date.month = ((arry[0] >> 4) & 0b00001100) + (arry[1] >> 6);
+			result.date.year = 2000 + (arry[4] & 0b01111111);
+			return result;
+		}
+
+		timestamp_t timestamp_t::parse_date( data_source_t arry ) {
+			assert( arry.size( ) >= 2 ); 
+			timestamp_t result;
+			result.date.day = arry[0] & 0b00011111;
+			result.date.month = ((arry[0] & 0b11100000) >> 4) + ((arry[1] & 0b10000000) >> 7);
+			result.date.year = 2000 + (arry[1] & 0b01111111);
+			return result;	
+		}
+
+		hist_bolus_normal::hist_bolus_normal( data_source_t data, pump_model_t pump_model ):
+			history_entry<hist_bolus_normal, 0x01>( this, std::move( data ), pump_model.larger ? 13 : 9, std::move( pump_model ) ),
+			m_timestamp_offset( pump_model.larger ? 8 : 4 ) { }
+		
+		hist_result_daily_total::hist_result_daily_total( data_source_t data, pump_model_t pump_model ):
+			history_entry<hist_result_daily_total, 0x07>( this, std::move( data ),  pump_model.larger ? 10 : 7, std::move( pump_model ) ),
+			m_timestamp_offset( pump_model.larger ? 8 : 4 ) { }
+
+
+		hist_change_sensor_setup::hist_change_sensor_setup( data_source_t data, pump_model_t pump_model ):
+			history_entry<hist_change_sensor_setup, 0x50>( this, std::move( data ), pump_model.has_low_suspend ? 41 : 37, std::move( pump_model ) ) { }
+
+		hist_change_bolus_wizard_setup::hist_change_bolus_wizard_setup( data_source_t data, pump_model_t pump_model ):
+			history_entry<hist_change_bolus_wizard_setup, 0x5A>( this, std::move( data ),  pump_model.larger ? 144 : 124, std::move( pump_model ) ) { } 
+
+		hist_change_bolus_wizard_estimate::hist_change_bolus_wizard_estimate( data_source_t data, pump_model_t pump_model ):
+				history_entry<hist_change_bolus_wizard_estimate, 0x5B>{ this, std::move( data ), static_cast<size_t>(pump_model.larger ? 22 : 20), pump_model } { } 
+
+		hist_unabsorbed_insulin::hist_unabsorbed_insulin( data_source_t data, pump_model_t pump_model ):
+				history_entry<hist_unabsorbed_insulin, 0x5C>( this, std::move( data ), (data[1] > 2 ? data[1] : 2), std::move( pump_model ) ) { } 
+
+		namespace {
+			template<typename ...Args>
+			std::unique_ptr<history_entry_obj> create_history_entry_impl( uint8_t opcode, Args&&... args ) {
+				return std::unique_ptr<history_entry_obj>( [&]( ) {
+				switch( opcode ) {
+					case 0X01: return new hist_bolus_normal( std::forward<Args>( args )... );
+					case 0X03: return new hist_prime( std::forward<Args>( args )... );
+					case 0X06: return new hist_alarm_pump( std::forward<Args>( args )... );
+					case 0X07: return new hist_result_daily_total( std::forward<Args>( args )... );
+					case 0X08: return new hist_change_basal_profile_pattern( std::forward<Args>( args )... );
+					case 0X09: return new hist_change_basal_profile( std::forward<Args>( args )... );
+					case 0X0A: return new hist_cal_bg_for_ph( std::forward<Args>( args )... );
+					case 0X0B: return new hist_alarm_sensor( std::forward<Args>( args )... );
+					case 0X0C: return new hist_clear_alarm( std::forward<Args>( args )... );
+					case 0X14: return new hist_select_basal_profile( std::forward<Args>( args )... );
+					case 0X16: return new hist_temp_basal_duration( std::forward<Args>( args )... );
+					case 0X17: return new hist_change_time( std::forward<Args>( args )... );
+					case 0X19: return new hist_pump_low_battery( std::forward<Args>( args )... );
+					case 0X1A: return new hist_battery( std::forward<Args>( args )... );
+					case 0X1E: return new hist_suspend( std::forward<Args>( args )... );
+					case 0X1F: return new hist_resume( std::forward<Args>( args )... );
+					case 0X21: return new hist_rewind( std::forward<Args>( args )... );
+					case 0X23: return new hist_change_child_block_enable( std::forward<Args>( args )... );
+					case 0X24: return new hist_change_max_bolus( std::forward<Args>( args )... );
+					case 0X26: return new hist_enable_disable_remote( std::forward<Args>( args )... );
+					case 0X2C: return new hist_change_max_basal( std::forward<Args>( args )... );
+					case 0X31: return new hist_change_bg_reminder_offset( std::forward<Args>( args )... );
+					case 0X32: return new hist_change_alarm_clock_time( std::forward<Args>( args )... );
+					case 0X33: return new hist_temp_basal( std::forward<Args>( args )... );
+					case 0X34: return new hist_pump_low_reservoir( std::forward<Args>( args )... );
+					case 0X35: return new hist_alarm_clock_reminder( std::forward<Args>( args )... );
+					case 0X3B: return new hist_questionable_3b( std::forward<Args>( args )... );
+					case 0X3C: return new hist_change_paradigm_linkid( std::forward<Args>( args )... );
+					case 0X3F: return new hist_bg_received( std::forward<Args>( args )... );
+					case 0X41: return new hist_exercise_marker( std::forward<Args>( args )... );
+					case 0X50: return new hist_change_sensor_setup( std::forward<Args>( args )... );
+					case 0X56: return new hist_change_sensor_rate_of_change_alert_setup( std::forward<Args>( args )... );
+					case 0X57: return new hist_change_bolus_scroll_step_size( std::forward<Args>( args )... );
+					case 0X5A: return new hist_change_bolus_wizard_setup( std::forward<Args>( args )... );
+					case 0X5B: return new hist_change_bolus_wizard_estimate( std::forward<Args>( args )... );
+					case 0X5C: return new hist_unabsorbed_insulin( std::forward<Args>( args )... );
+					case 0X5E: return new hist_change_variable_bolus( std::forward<Args>( args )... );
+					case 0X5F: return new hist_change_audio_bolus( std::forward<Args>( args )... );
+					case 0X60: return new hist_change_bg_reminder_enable( std::forward<Args>( args )... );
+					case 0X61: return new hist_change_alarm_clock_enable( std::forward<Args>( args )... );
+					case 0X62: return new hist_change_temp_basal_type( std::forward<Args>( args )... );
+					case 0X63: return new hist_change_alarm_notify_mode( std::forward<Args>( args )... );
+					case 0X64: return new hist_change_time_format( std::forward<Args>( args )... );
+					case 0X65: return new hist_change_reservoir_warning_time( std::forward<Args>( args )... );
+					case 0X66: return new hist_change_bolus_reminder_enable( std::forward<Args>( args )... );
+					case 0X67: return new hist_change_bolus_reminder_time( std::forward<Args>( args )... );
+					case 0X68: return new hist_delete_bolus_reminder_time( std::forward<Args>( args )... );
+					case 0X6A: return new hist_delete_alarm_clock_time( std::forward<Args>( args )... );
+					case 0X6D: return new hist_model_522_result_totals( std::forward<Args>( args )... );
+					case 0X6E: return new hist_sara_6e( std::forward<Args>( args )... );
+					case 0X6F: return new hist_change_carb_units( std::forward<Args>( args )... );
+					case 0X7B: return new hist_basal_profile_start( std::forward<Args>( args )... );
+					case 0X7C: return new hist_change_watch_dog_enable( std::forward<Args>( args )... );
+					case 0X7D: return new hist_change_other_device_id( std::forward<Args>( args )... );
+					case 0X81: return new hist_change_watch_dog_marriage_profile( std::forward<Args>( args )... );
+					case 0X82: return new hist_delete_other_device_id( std::forward<Args>( args )... );
+					case 0X83: return new hist_change_capture_event_enable( std::forward<Args>( args )... );
+					default: {
+						return nullptr;
+					}
+				}
+			}( ) );
+		}
+
+		std::unique_ptr<history_entry_obj> create_history_entry( data_source_t & data, pump_model_t pump_model ) {
+			auto result = create_history_entry_impl( data[0], data, std::move( pump_model ) );
+			if( data.size( ) < result->size( ) ) {
+				return nullptr;
+			}
+			next( data, result->size( ) );
+			return result;
+		}
+	}	// namespace history
+}	// namespace daw
+
---
+++ include/history_pages.h
@@ -0,0 +1,239 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2014-2015 Darrell Wright
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files( the "Software" ), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include <boost/optional.hpp>
+#include <daw/daw_range.h>
+#include <cstdint>
+
+namespace daw {
+	namespace history {
+		using data_source_t = daw::range::Range<uint8_t *>;
+		struct pump_model_t {
+			uint16_t generation;
+			bool larger;
+			bool has_low_suspend;
+			uint8_t strokes_per_unit;
+
+			pump_model_t( ) = delete;
+			pump_model_t( std::string const & model );
+
+			virtual ~pump_model_t( );
+			pump_model_t( pump_model_t const & ) = default;
+			pump_model_t( pump_model_t && ) = default;
+			pump_model_t & operator=( pump_model_t const & ) = default;
+			pump_model_t & operator=( pump_model_t && ) = default;
+		};	// pump_model_t
+
+		struct timestamp_t {
+			struct {
+				uint16_t year;
+				uint8_t month;
+				uint8_t day;
+			} date;
+
+			struct time_t {
+				uint8_t hour;
+				uint8_t minute;
+				uint8_t second;
+			};
+			boost::optional<time_t> time;
+			
+			bool has_time( ) const;
+			
+			static timestamp_t parse_timestamp( data_source_t arry );
+			static timestamp_t parse_date( data_source_t arry );
+		};	// timestamp_t
+
+		class history_entry_obj {
+			uint8_t m_opcode; 
+			data_source_t m_data;
+			size_t m_size;
+		protected:
+			history_entry_obj( data_source_t data, size_t data_size, pump_model_t );
+		public:
+			virtual ~history_entry_obj( );
+			history_entry_obj( history_entry_obj const & ) = default;
+			history_entry_obj( history_entry_obj && ) = default;
+			history_entry_obj & operator=( history_entry_obj const & ) = default;
+			history_entry_obj & operator=( history_entry_obj && ) = default;
+			virtual size_t timestamp_offset( ) const = 0;
+			virtual size_t timestamp_size( ) const = 0;
+
+			std::tuple<uint8_t, size_t, size_t> register_event_type( ) const;
+
+			virtual boost::optional<timestamp_t> timestamp( ) const;
+
+			uint8_t opcode( ) const;
+			
+			data_source_t & data( );
+			data_source_t const & data( ) const;
+			virtual size_t & size( );
+			virtual size_t const & size( ) const;
+		};	// history_entry_obj
+
+		template<typename Derived, uint8_t child_opcode>
+		class history_entry: public history_entry_obj {
+			Derived * child;
+		protected:
+			history_entry( Derived * derived, data_source_t data, size_t data_size, pump_model_t pump_model ): 
+					history_entry_obj{ std::move( data ), data_size, std::move( pump_model ) }, 
+					child( derived ) {
+
+				assert( nullptr != child );
+			}
+
+		public:
+			virtual ~history_entry( ) = default;
+			history_entry( history_entry const & ) = default;
+			history_entry( history_entry && ) = default;
+			history_entry & operator=( history_entry const & ) = default;
+			history_entry & operator=( history_entry && ) = default;
+
+			size_t timestamp_offset( ) const override {
+				return child->m_timestamp_offset;
+			}
+		
+			size_t timestamp_size( ) const override {
+				return child->m_timestamp_size;
+			}
+
+		};	// history_entry
+
+		template<uint8_t child_opcode, size_t child_size = 7, size_t child_timestamp_offset = 2, size_t child_timestamp_size = 5>
+		class history_entry_static: public history_entry<history_entry_static<child_opcode, child_size, child_timestamp_offset>, child_opcode> {
+			static size_t const m_timestamp_offset = child_timestamp_offset;
+			static size_t const m_timestamp_size = child_timestamp_size;
+			
+			history_entry_static( data_source_t data, pump_model_t pump_model ):
+					history_entry<history_entry_static<child_opcode, child_size, child_timestamp_offset>, child_opcode>{ *this, std::move( data ), child_size, std::move( pump_model ) } { }
+
+			virtual ~history_entry_static( ) = default;
+			history_entry_static( history_entry_static const & ) = default;
+			history_entry_static( history_entry_static && ) = default;
+			history_entry_static & operator=( history_entry_static const & ) = default;
+			history_entry_static & operator=( history_entry_static && ) = default;
+		};	// history_entry_static	
+
+		// Known History Entries in order of opcode
+		struct hist_bolus_normal: public history_entry<hist_bolus_normal, 0x01> {
+			size_t m_timestamp_offset;
+			hist_bolus_normal( data_source_t data, pump_model_t pump_model );
+			virtual ~hist_bolus_normal( );
+			hist_bolus_normal( hist_bolus_normal const & ) = default;
+			hist_bolus_normal( hist_bolus_normal && ) = default;
+			hist_bolus_normal & operator=( hist_bolus_normal const & ) = default;
+			hist_bolus_normal & operator=( hist_bolus_normal && ) = default;
+
+		};	// hist_bolus_normal
+
+		using hist_prime = history_entry_static<0x03, 10, 5>;
+		using hist_alarm_pump = history_entry_static<0x06, 9, 4>;
+
+		struct hist_result_daily_total: public history_entry<hist_result_daily_total, 0x07> {
+			static size_t const m_timestamp_size = 2;
+			size_t m_timestamp_offset;
+
+			hist_result_daily_total( data_source_t data, pump_model_t pump_model );
+
+			virtual ~hist_result_daily_total( );
+
+			hist_result_daily_total( hist_result_daily_total const & ) = default;
+			hist_result_daily_total( hist_result_daily_total && ) = default;
+			hist_result_daily_total & operator=( hist_result_daily_total const & ) = default;
+			hist_result_daily_total & operator=( hist_result_daily_total && ) = default;
+		};	// hist_result_daily_total
+
+		using hist_change_basal_profile_pattern = history_entry_static<0x08, 152>;
+		using hist_change_basal_profile = history_entry_static<0x09, 152>;
+		using hist_cal_bg_for_ph = history_entry_static<0x0A>;
+		using hist_alarm_sensor = history_entry_static<0x0B, 8>;
+		using hist_clear_alarm = history_entry_static<0x0C>;
+		using hist_select_basal_profile = history_entry_static<0x14>;
+		using hist_temp_basal_duration = history_entry_static<0x16>;
+		using hist_change_time = history_entry_static<0x17, 14, 9>;
+		using hist_pump_low_battery = history_entry_static<0X19>;
+		using hist_battery = history_entry_static<0X1A>;
+		using hist_suspend = history_entry_static<0X1E>;
+		using hist_resume = history_entry_static<0X1F>;
+		using hist_rewind = history_entry_static<0X21>;
+		using hist_change_child_block_enable = history_entry_static<0X23>;
+		using hist_change_max_bolus = history_entry_static<0X24>;
+		using hist_enable_disable_remote = history_entry_static<0X26, 21>;
+		using hist_change_max_basal = history_entry_static<0X2C>;
+		using hist_change_bg_reminder_offset = history_entry_static<0X31>;
+		using hist_change_alarm_clock_time = history_entry_static<0X32, 14>;
+		using hist_temp_basal = history_entry_static<0X33, 8>;
+		using hist_pump_low_reservoir = history_entry_static<0X34>;
+		using hist_alarm_clock_reminder = history_entry_static<0X35>;
+		using hist_questionable_3b = history_entry_static<0X3B>;
+		using hist_change_paradigm_linkid = history_entry_static<0X3C, 21>;
+		using hist_bg_received = history_entry_static<0X3F, 10>;
+		using hist_exercise_marker = history_entry_static<0X41, 8>;
+
+		using hist_change_sensor_rate_of_change_alert_setup = history_entry_static<0X56, 12>;
+		using hist_change_bolus_scroll_step_size = history_entry_static<0X57>;
+
+		struct hist_change_sensor_setup: public history_entry<hist_change_sensor_setup, 0x50> {
+			hist_change_sensor_setup( data_source_t data, pump_model_t pump_model );
+		};	// hist_change_sensor_setup
+
+		struct hist_change_bolus_wizard_setup: public history_entry<hist_change_bolus_wizard_setup, 0x5A> {
+			hist_change_bolus_wizard_setup( data_source_t data, pump_model_t pump_model );
+		};	// hist_change_bolus_wizard_setup
+
+		struct hist_change_bolus_wizard_estimate: public history_entry<hist_change_bolus_wizard_estimate, 0x5B> {
+			hist_change_bolus_wizard_estimate( data_source_t data, pump_model_t pump_model );
+		};	// hist_change_bolus_wizard_estimate
+
+		struct hist_unabsorbed_insulin: public history_entry<hist_unabsorbed_insulin, 0x5C> {
+			hist_unabsorbed_insulin( data_source_t data, pump_model_t pump_model );
+		};	// hist_unabsorbed_insulin
+
+		using hist_change_variable_bolus = history_entry_static<0X5E>;
+		using hist_change_audio_bolus = history_entry_static<0X5F>;
+		using hist_change_bg_reminder_enable = history_entry_static<0X60>;
+		using hist_change_alarm_clock_enable = history_entry_static<0X61>;
+		using hist_change_temp_basal_type = history_entry_static<0X62>;
+		using hist_change_alarm_notify_mode = history_entry_static<0X63>;
+		using hist_change_time_format = history_entry_static<0X64>;
+		using hist_change_reservoir_warning_time = history_entry_static<0X65>;
+		using hist_change_bolus_reminder_enable = history_entry_static<0X66>;
+		using hist_change_bolus_reminder_time = history_entry_static<0X67>;
+		using hist_delete_bolus_reminder_time = history_entry_static<0X68, 9>;
+		using hist_delete_alarm_clock_time = history_entry_static<0X6A, 14>;
+		using hist_model_522_result_totals = history_entry_static<0X6D, 44, 1, 2>;
+		using hist_sara_6e = history_entry_static<0X6E, 52, 1, 2>;
+		using hist_change_carb_units = history_entry_static<0X6F>;
+		using hist_basal_profile_start = history_entry_static<0X7B, 10>;
+		using hist_change_watch_dog_enable = history_entry_static<0X7C>;
+		using hist_change_other_device_id = history_entry_static<0X7D, 37>;
+		using hist_change_watch_dog_marriage_profile = history_entry_static<0X81, 12>;
+		using hist_delete_other_device_id = history_entry_static<0X82, 12>;
+		using hist_change_capture_event_enable = history_entry_static<0X83>;
+
+
+		std::unique_ptr<history_entry_obj> create_history_entry( data_source_t & data, pump_model_t pump_model );
+	}	// namespace history
+}	// namespace daw
+
